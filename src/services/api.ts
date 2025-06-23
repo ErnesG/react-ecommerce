7449aced512c3bc1484a5491bfc15c6ee3f41b2e
LATEAM99 <egonzl92@outlook.com>
2025-06-22 18:41:57 -0700

    api

---
+++ src/services/api.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+import type { Product } from "../types";
+
+const API_BASE_URL = "https://fakestoreapi.com"
+const api = axios.create({
+    baseURL: API_BASE_URL,
+    timeout: 10000
+});
+
+export const productService = {
+    getAllProducts: async (): Promise<Product[]> => {
+        const response = await api.get<Product[]>('/products');
+        return response.data;
+    },
+    getProductById: async (id: number): Promise<Product> => {
+        const response = await api.get<Product>(`/products/${id}`);
+        return response.data;
+    },
+    getCategories: async (): Promise<string[]> => {
+        const response = await api.get<string[]>('/products/categories');
+        return response.data;
+    },
+    getProductsByCategory: async (category: string): Promise<Product[]> => {
+        const response = await api.get<Product[]>(`/products/category/${category}`);
+        return response.data;
+    },
+
+}
